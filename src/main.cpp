8748cc5d581a4b8bec8a3832ffc1faec2f506153
i7628916 <i7628916@bournemouth.ac.uk>
2016-02-22 11:46:15 +0000

    COMMIT

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <SDLOpenGL.h>
+#include <ngl/NGLInit.h>
+
+void initializeGL();
+void renderGL();
+
 
 int main(int argc, char *argv[])
 {
+    SDLOpenGL window("test",0,0,1024,720);
     std::cout<<"working\n";
+    window.makeCurrent();
+    initializeGL();
+    while(true)
+    {
+        renderGL();
+        window.swapWindow();
+        SDL_Delay(10);
+    }
+}
+
+void initializeGL()
+{
+    ngl::NGLInit::instance();
+    glClearColor(0.8,0.8,0.8,1.0);
+
+}
+
+void renderGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 }
